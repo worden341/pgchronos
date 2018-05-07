c843e070b778320ad54c6b301f8e240a236a17f8
Eric Worden <worden.eric@gmail.com>
2018-05-06 17:49:54 -0700

    test null arguments, empty arrays, and single-element arrays

--- test/test_result_single_reduce_inclusive_build.sql
+++ test/test_result_single_reduce_inclusive_build.sql
@@ -1,4 +1,55 @@
-with combos as 
+insert into test_result (sequence_id, operator, lower_inc1, upper_inc1, result)
+select
+    '' as id,
+    '+' op
+    reduce(array[]::tstzrange[]) result
+;
+
+with combos as
+(
+    select
+        ts.id,
+        ts.r1_lower,
+        ts.r1_upper,
+        inc1.lower_inc1,
+        inc1.upper_inc1
+    from
+        test_sequence ts
+        cross join
+        (
+            values
+                (true, true),
+                (true, false),
+                (false, true),
+                (false, false)
+        ) inc1 (lower_inc1, upper_inc1)
+    where
+        length(ts.id) = 2
+        and not
+        (
+            --these are invalid values:
+            (r1_lower is null and lower_inc1)
+            or (r1_upper is null and upper_inc1)
+        )
+)
+insert into test_result (sequence_id, operator, lower_inc1, upper_inc1, result)
+select
+--     format('%s%s%s',
+--         case when lower_inc1 then '[' else '(' end,
+--         substr(id, 1, 2),
+--         case when upper_inc1 then ']' else ')' end
+--     ) id,
+    id,
+    '+' op,
+    lower_inc1,
+    upper_inc1
+    reduce(array[tstzrange(r1_lower, r1_upper, pgchronos_range_inclusiveness_text(lower_inc1, upper_inc1))]) result
+from combos
+order by 1,3,4,5,6
+--order by 1
+;
+
+with combos as
 (
     select
         ts.id,
@@ -29,6 +80,7 @@ with combos as
                 (false, false)
         ) inc2 (lower_inc2, upper_inc2)
     where true
+        and length(ts.id) = 4
         and not
         (
             --these are invalid values:
@@ -41,7 +93,7 @@ with combos as
 )
 insert into test_result (sequence_id, operator, lower_inc1, upper_inc1, lower_inc2, upper_inc2, result)
 select
---     format('%s%s%s %s%s%s', 
+--     format('%s%s%s %s%s%s',
 --         case when lower_inc1 then '[' else '(' end,
 --         substr(id, 1, 2),
 --         case when upper_inc1 then ']' else ')' end,
--- test/tests.sql
+++ test/tests.sql
@@ -1,21 +1,21 @@
 \pset format unaligned
 \pset tuples_only true
 
-\set ON_ERROR_ROLLBACK 1
+\set ON_ERROR_ROLLBACK true
 \set ON_ERROR_STOP true
-\set QUIET 1
+\set QUIET true
 
 BEGIN;
 
 \ir test_tables.sql
+\copy test_result from 'test_result.copy'
 
 create or replace function test_f1()
 returns setof text
 language plpgsql as $func$
 declare
-    v_record record;
-    v_row test_result%rowtype;
     v_id text;
+    v_result_id integer;
     v_r1_lower timestamptz;
     v_r1_upper timestamptz;
     v_r2_lower timestamptz;
@@ -26,22 +26,43 @@ declare
     v_upper_inc2 boolean;
     v_result tstzrange[];
 begin
-    for v_id, v_r1_lower, v_r1_upper, v_r2_lower, v_r2_upper, v_lower_inc1, v_upper_inc1, v_lower_inc2, v_upper_inc2, v_result in
-    select ts.id, r1_lower, r1_upper, r2_lower, r2_upper, lower_inc1, upper_inc1, lower_inc2, upper_inc2, tr.result
+    return next ok((reduce(null::tstzrange[]) is null), 'reduce(null) returns null');
+
+    for v_result_id, v_id, v_r1_lower, v_r1_upper, v_r2_lower, v_r2_upper, v_lower_inc1, v_upper_inc1, v_lower_inc2, v_upper_inc2, v_result in
+    select tr.id, ts.id, r1_lower, r1_upper, r2_lower, r2_upper, lower_inc1, upper_inc1, lower_inc2, upper_inc2, tr.result
     from
         test_sequence ts
         join test_result tr on ts.id = tr.sequence_id
+    where
+        tr.operator = '+'
+        --and length(ts.id) = 4
     order by tr.id LOOP
-        return next is((reduce(array[tstzrange(v_r1_lower, v_r1_upper, pgchronos_range_inclusiveness_text(v_lower_inc1, v_upper_inc1)), tstzrange(v_r2_lower, v_r2_upper, pgchronos_range_inclusiveness_text(v_lower_inc2, v_upper_inc2))]))::text, v_result::text,
-            format('reduce %s%s%s %s%s%s',
-                case when v_lower_inc1 then '[' else '(' end,
-                substr(v_id, 1, 2),
-                case when v_upper_inc1 then ']' else ')' end,
-                case when v_lower_inc2 then '[' else '(' end,
-                substr(v_id, 3),
-                case when v_upper_inc2 then ']' else ')' end
-            )
-        );
+        if length(v_id) = 4 then
+            return next is((reduce(array[tstzrange(v_r1_lower, v_r1_upper, pgchronos_range_inclusiveness_text(v_lower_inc1, v_upper_inc1)), tstzrange(v_r2_lower, v_r2_upper, pgchronos_range_inclusiveness_text(v_lower_inc2, v_upper_inc2))]))::text, v_result::text,
+                format('reduce %s%s%s %s%s%s id=%s',
+                    case when v_lower_inc1 then '[' else '(' end,
+                    substr(v_id, 1, 2),
+                    case when v_upper_inc1 then ']' else ')' end,
+                    case when v_lower_inc2 then '[' else '(' end,
+                    substr(v_id, 3),
+                    case when v_upper_inc2 then ']' else ')' end,
+                    v_result_id
+                )
+            );
+        elsif length(v_id) = 2 then
+            return next is((reduce(array[tstzrange(v_r1_lower, v_r1_upper, pgchronos_range_inclusiveness_text(v_lower_inc1, v_upper_inc1))]))::text, v_result::text,
+                format('reduce %s%s%s id=%s',
+                    case when v_lower_inc1 then '[' else '(' end,
+                    substr(v_id, 1, 2),
+                    case when v_upper_inc1 then ']' else ')' end,
+                    v_result_id
+                )
+            );
+        elsif length(v_id) = 0 then
+            return next is((reduce(array[]::tstzrange[]))::text, v_result::text,
+                format('reduce empty array id=%s', v_result_id)
+            );
+        end if;
     end loop;
 end;
 $func$;
@@ -49,3 +70,10 @@ $func$;
 select * from runtests();
 
 ROLLBACK;
+
+\pset format
+\pset tuples_only
+
+\unset ON_ERROR_ROLLBACK
+\unset ON_ERROR_STOP
+\unset QUIET
